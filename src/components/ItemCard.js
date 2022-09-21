4c5083235f829950856a231347f09815390cd607
crespire <li.simmon@gmail.com>
2022-09-21 17:06:03 -0400

    wip: add Item display component stubs

---
+++ src/components/ItemCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ItemCard() {
+  return (
+    <div className="">
+      ItemCard
+    </div>
+  );
+}
+
+export default ItemCard;
---
+++ src/components/ItemDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ItemDetail() {
+  return (
+    <div className="">
+      ItemDetail
+    </div>
+  );
+}
+
+export default ItemDetail;
