f550a9428c6b7391e3863324b26e8b3084893c21
crespire <li.simmon@gmail.com>
2022-09-21 12:14:45 -0400

    wip: Adding logic to Store component to realize implementation

---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Display() {
+  return (
+    <div className="">
+      Display
+    </div>
+  );
+}
+
+export default Display;
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import inventory from "../Inventory.json";
+import Sidebar from './Sidebar';
+import Display from './Display';
 
 function Store() {
+  const [cart, setCart] = useState([]);
+  const [checkoutFlow, setCheckoutflow] = useState(false);
+
+  const addItem = (id, quantity) => {
+    console.log(`Adding ${quantity} of itemID ${id}`);
+  };
+
+  const removeItem = (id) => {
+    console.log(`Removing itemID ${id}`);
+    // Probably splice based on ID
+  };
+
+  const updateItem = (id, quantity) => {
+    if (quantity === 0) {
+      removeItem(id);
+    } else {
+      console.log(`Updating to ${quantity} of itemID ${id}`);
+      // Probably a splice with replace is required here.
+    }    
+  }
+
   return (
-    <div className="App">
-      Store page
+    <div className="">
+      <Sidebar />
+      <Display />
     </div>
   );
 }
