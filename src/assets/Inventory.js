65ac2a8781b78580a94f8313daa81625211f49d8
crespire <li.simmon@gmail.com>
2022-09-21 17:06:34 -0400

    fix: update inventory from json to JS object and add images

--- src/assets/Inventory.js
+++ src/assets/Inventory.js
@@ -1,11 +1,11 @@
-{
+export const inventory = {
   "dog-kibble": {
-    "image": "kibble",
+    "image": require('./item_imgs/kibble.jpg'),
     "name": "Dog Kibble",
     "description": "A healthy kibble for your pup!"
   },
   "dog-treats": {
-    "image": "treat",
+    "image": require('./item_imgs/treat.jpg'),
     "name": "Dog Treats",
     "description": "A package of delicious treats, yum!"
   }
