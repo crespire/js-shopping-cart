b247ac1c16521fc5e7696b5b0365b771e41761e6
crespire <li.simmon@gmail.com>
2022-09-22 09:17:07 -0400

    feat: add dog bed to inventory

--- src/assets/Inventory.js
+++ src/assets/Inventory.js
@@ -2,11 +2,16 @@ export const inventory = {
   "dog-kibble": {
     "image": require('./item_imgs/kibble.jpg'),
     "name": "Dog Kibble",
-    "description": "A healthy kibble for your pup!"
+    "description": "A healthy kibble for your pup!",
   },
   "dog-treats": {
     "image": require('./item_imgs/treat.jpg'),
     "name": "Dog Treats",
     "description": "A package of delicious treats, yum!"
+  },
+  "dog-bed": {
+    "image": require('./item_imgs/bed.jpg'),
+    "name": "Dog Bed",
+    "description": "A comfortable bed for the pooch."
   }
 }
