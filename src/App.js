eaddea3ec81aa787a0ed5fe029a694fc97c506da
crespire <li.simmon@gmail.com>
2022-09-23 13:34:41 -0400

    feat: add Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from "react-router-dom";
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="flex flex-col justify-center align-center h-full min-h-screen max-h-screen">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="flex basis-0 justify-end items-center border-t border-solid border-black p-4 space-x-4">
+      <span>Simmon "Crespire" Li &copy; 2022</span>
+    </footer>
+  );
+}
+
+export default Footer;
