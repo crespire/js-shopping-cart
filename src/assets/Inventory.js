e00b68bbdf9f6c4a889957e7b0196c05fe66b1f6
crespire <li.simmon@gmail.com>
2022-09-23 14:34:36 -0400

    chore: add more inventory items

--- src/assets/Inventory.js
+++ src/assets/Inventory.js
@@ -29,16 +29,52 @@ export const inventory = {
     "cost": 10,
     "description": "Very fancy, put your dog's tag on this collar."
   },
+  "dog-leash": {
+    "image": require('./item_imgs/dog_leash.jpg'),
+    "name": "Dog Leash",
+    "cost": 25,
+    "description": "A stylish and sturdy leash for dogs."
+  },
   "dog-chew-toy": {
     "image": require('./item_imgs/dog_chew_toy.jpg'),
     "name": "Dog Chew Toy",
     "cost": 8,
     "description": "A toy for your pup to munch on, fill it with treats."
   },
+  "dog-bags": {
+    "image": require('./item_imgs/dog_bags.jpg'),
+    "name": "Dog Bags",
+    "cost": 10,
+    "description": "Bags to clean up after your doggo."
+  },
   "cat-food": {
     "image": require('./item_imgs/cat_food.jpg'),
     "name": "Cat Food",
     "cost": 5,
     "description": "A can of delicious food for kitty!"
+  },
+  "cat-bed": {
+    "image": require('./item_imgs/cat_bed.jpg'),
+    "name": "Cat Cuddler Bed",
+    "cost": 45,
+    "description": "A cozy cuddle bed for cats."
+  },
+  "cat-ribbon-toy": {
+    "image": require('./item_imgs/cat_ribbon_toy.jpg'),
+    "name": "Cat Ribbon Toy",
+    "cost": 10,
+    "description": "Can your cat catch this toy?"
+  },
+  "cat-litter-box": {
+    "image": require('./item_imgs/cat_litter_box.jpg'),
+    "name": "Cat Litter Box",
+    "cost": 40,
+    "description": "A high sided litter box for pooping in style."
+  },
+  "cat-litter": {
+    "image": require('./item_imgs/cat_litter.jpg'),
+    "name": "Cat Litter",
+    "cost": 25,
+    "description": "Litter for the cat's business."
   },
 }
